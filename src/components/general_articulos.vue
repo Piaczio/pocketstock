d0cd4808b547b222b3ef08be18dd15d4242cd737
Piaczio <trotroyu@gmail.com>
2021-02-09 17:49:18 -0700

    hot fix menu

--- src/components/general_articulos.vue
+++ src/components/general_articulos.vue
@@ -8,15 +8,28 @@
             <thead>
               <tr>
                 <th class="text-left">Articulo</th>
-                <th class="text-left">Descripción</th>
                 <th class="text-left">Cantidad</th>
+                <th class="text-left">Descripción</th>
+                <th class="text-left">Tipo</th>
+                <th class="text-left">Marca</th>
+                <th class="text-left">Proveedor</th>
+                <th class="text-left">Status</th>
+                <th class="text-left">Ubicación</th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="item in articulosArray" :key="item.itemid">
                 <td class="table-dist">{{ item.nombre_articulo }}</td>
-                <td class="table-dist">{{ item.descripcion_articulo }}</td>
                 <td class="table-dist">{{ item.cantidad_articulo }}</td>
+                <td class="table-dist">{{ item.descripcion_articulo }}</td>
+                <td class="table-dist">{{ item.tipo_articulo }}</td>
+                <td>{{ item.marca_articulo }}</td>
+                <td class="table-dist">{{ item.proveedor_articulo }}</td>
+                <td class="table-dist">{{ item.status_articulo }}</td>
+                <td class="table-dist">
+                  {{ item.ubicacion_rack_articulo }},
+                  {{ item.ubicacion_traveseaño_articulo }}
+                </td>
               </tr>
             </tbody>
           </template>
@@ -38,13 +51,22 @@ export default {
     axios
       .get("http://127.0.0.1:8000/api/articulo")
       .then((response) => {
+        console.log("this -> ", response.data);
         let articulos = response.data;
+
         articulos.forEach((element) => {
           let datos = {
             nombre_articulo: element.nombre_articulo,
-            descripcion_articulo: element.descripcion_articulo,
             cantidad_articulo: element.cantidad_articulo,
+            descripcion_articulo: element.descripcion_articulo,
+            marca_articulo: element.nombre_marca,
+            tipo_articulo: element.name_tipo,
+            proveedor_articulo: element.nombre_proveedor,
+            status_articulo: element.nombre_status,
+            ubicacion_rack_articulo: element.rack,
+            ubicacion_traveseaño_articulo: element.traveseaño,
           };
+
           if (!datos) return;
           this.articulosArray.push(datos);
         });
@@ -55,12 +77,13 @@ export default {
 </script>
 <style scoped>
 .foodtable {
-  padding-left: 20%;
+  padding-left: 17%;
   padding-top: 0%;
 
-  padding-right: 20%;
+  padding-right: 5%;
 }
 .table-dist {
   text-align: left;
 }
 </style>
+
